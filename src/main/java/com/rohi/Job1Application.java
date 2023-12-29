aa7739927bfe4a473c7d7ed73f60cdfc7e2315cb
Damavarupu Rohith <damavarapurohith334>
2023-12-29 15:23:39 +0530

    first commit

---
+++ src/main/java/com/rohi/Job1Application.java
@@ -0,0 +1,13 @@
+package com.rohi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Job1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rohi/Job1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Job1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
